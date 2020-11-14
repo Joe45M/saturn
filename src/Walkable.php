89f55f40054cebd28192970ea06809782cda5b93
Joe <joe45moses@gmail.com>
2020-11-14 15:27:21 +0000

    Added Walkable to walk undetermind arrays

---
+++ src/Walkable.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace Joem\Saturn;
+
+
+trait Walkable
+{
+
+    /**
+     * Walk an array and organise it's potential layouts.
+     *
+     * A walkable var is an array that can contain an array, which contains either key-value parameters, or a
+     * multi-level array which contains further arrays - which THEN contain key-value parameters.
+     *
+     * Walking these values means we can easily manage parameters in methods, without complicating how developers must
+     * provide the parameters.
+     *
+     * @param array $walk
+     * @return array
+     */
+    public static function walk(Array $walk) {
+
+        $wpArgs = [];
+
+        foreach($walk as $key => $step) {
+
+            if(gettype($step) == 'array') {
+                foreach($step as $stepKey => $stepValue) {
+                    $wpArgs[] = array(
+                        'key' => $stepKey,
+                        'value' => $stepValue
+                    );
+                }
+            } else {
+                $wpArgs = array(
+                    'key' => $key,
+                    'value' => $step
+                );
+            }
+        }
+
+        return $wpArgs;
+    }
+}
